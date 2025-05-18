a9a3411cf208fc2d0bf2653a1bc32587803078cb
madhu99priya <157110469+madhu99priya@users.noreply.github.com>
2025-05-18 23:30:08 +0530

    model training script added

--- model/model.py
+++ model/model.py
@@ -1,3 +1,17 @@
+# from onnx2torch import convert
+# import onnx
+# import torch
+
+# # Load the ONNX model
+# onnx_model = onnx.load("./model.onnx")
+
+# # Convert to PyTorch model
+# torch_model = convert(onnx_model)
+
+# torch.save(torch_model, 'recovered_model.pth')
+# print("✅ Model saved as 'recovered_model.pth' successfully!")
+
+
 from onnx2torch import convert
 import onnx
 import torch
@@ -8,5 +22,8 @@ onnx_model = onnx.load("./model.onnx")
 # Convert to PyTorch model
 torch_model = convert(onnx_model)
 
-torch.save(torch_model, 'recovered_model.pth')
-print("✅ Model saved as 'recovered_model.pth' successfully!")
+# (Optional) Save as state_dict so it's easier to manage later
+torch.save(torch_model.state_dict(), "model_weights.pth")
+print("✅ Model saved as 'model_weights.pth' successfully!")
+
+# print(torch_model)
